7d62edc8ff7a596cdf29f41b845bcb1272a94e44
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-02 18:36:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,7 +27,7 @@ def game_hash ()
       :team_name => "", 
       :colors => [], 
       :players => [
-         {:player_name => "Alan Anderson", :number => 0, :shoe => 16 , :points => 22, 
+         {:player_name => "Jeff Adrien", :number => 4, :shoe => 18 , :points => 10, 
           :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1}, 
           
          {:player_name => "Reggie Evans", :number => 30, :shoe =>14 , :points => 12, 
