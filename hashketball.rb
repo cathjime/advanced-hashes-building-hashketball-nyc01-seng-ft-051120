9585733a0dcd5e9fdc66dca5f945f74439a37401
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-02 17:58:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,19 @@ def game_hash ()
   {
     :home => {:team_name => "", 
               :colors => [], 
-              :players => [{}]}, 
+              :players => [{:player_name => ,
+                            :number => ,
+                            :shoe => , 
+                            :points => , 
+                            :rebounds => , 
+                            
+                            
+                
+              }]}, 
+              
+              
+              
+              
     :away => {:team_name => "", 
               :colors => [], 
               :players => [{}]}
