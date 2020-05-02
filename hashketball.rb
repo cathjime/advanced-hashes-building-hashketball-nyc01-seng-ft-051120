f5fe3e45955163dc77b4ff5315e68c530b87535c
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-02 18:37:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,7 +30,7 @@ def game_hash ()
          {:player_name => "Jeff Adrien", :number => 4, :shoe => 18 , :points => 10, 
           :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2}, 
           
-         {:player_name => "Reggie Evans", :number => 30, :shoe =>14 , :points => 12, 
+         {:player_name => "Bismack Biyombo", :number => 0, :shoe => 16 , :points => 12, 
           :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
           
          {:player_name => "Brook Lopez", :number => 11, :shoe =>17 , :points => 17, 
