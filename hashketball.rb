064a0830cc5a8d09d7545c2689de7e3dc85364d0
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-02 18:34:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,11 +15,7 @@ def game_hash ()
          {:player_name => "Brook Lopez", :number => 11, :shoe =>17 , :points => 17, 
           :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
           
-          {:player_name => "Alan Anderson", :number => 0, :shoe => 16 , :points => 22, 
-          :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
-          
-          
-         {:player_name => "Mason Plumee", :number => 1, :shoe => 19 , :points => 26, 
+         {:player_name => "Mason Plumlee", :number => 1, :shoe => 19 , :points => 26, 
           :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
           
          {:player_name => "Jason Terry", :number => 31, :shoe =>15 , :points => 19, 
