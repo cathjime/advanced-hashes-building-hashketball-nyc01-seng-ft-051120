45cdf7bb88166814c94c3fb99373882d30e080c3
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-05-02 18:38:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,7 +36,7 @@ def game_hash ()
          {:player_name => "DeSagna Diop", :number => 2, :shoe =>14 , :points => 24, 
           :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
           
-         {:player_name => "Ben Gordon", :number => 1, :shoe => 19 , :points => 26, 
+         {:player_name => "Ben Gordon", :number => 8, :shoe => 15 , :points => 33, 
           :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
           
          {:player_name => "Jason Terry", :number => 31, :shoe =>15 , :points => 19, 
